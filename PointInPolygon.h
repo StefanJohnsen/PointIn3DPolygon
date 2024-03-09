12bc252ecdbfd928f9fded50aa97fb60a4d8aa6c
Falcon Coding <stefan.johnsen@outlook.com>
2024-03-10 00:47:16 +0100

    Update PointInPolygon.h
    Add Licence text

--- PointInPolygon.h
+++ PointInPolygon.h
@@ -5,14 +5,12 @@
 
   C++ Template Class for testing if a point is inside a polygon
 
-  Copyright (c) 2024 FalconCoding - Stefan Falk Johnsen
+  Copyright (c) 2024 FalconCoding
 
-  This software is released under the terms of the
-  GNU General Public License v3.0. Details and terms of this
-  license can be found at: https://www.gnu.org/licenses/gpl-3.0.html
-  For those who require the freedom to operate without the
-  constraints of the GPL, a commercial license can be obtaining by
-  contacting the author at stefan.johnsen@outlook.com
+  Author: Stefan Johnsen
+  Email: stefan.johnsen@outlook.com
+
+  This software is released under the MIT License.
  */
 
 #ifndef POINT_IN_POLYGON
