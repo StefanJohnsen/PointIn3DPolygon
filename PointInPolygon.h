e781b4823896373dde7ff75d8a941bfe23910b8d
StefanJohnsen <stefan.johnsen@outlook.com>
2024-03-10 15:57:15 +0100

    Refactoring

--- PointInPolygon.h
+++ PointInPolygon.h
@@ -46,7 +46,7 @@ namespace pip //point in polygon
 
 	inline bool zero(const double& f)
 	{
-		return equal(f, 0.0);
+		return equal(f, 0);
 	}
 
 	inline bool zero(const float& f)
@@ -57,9 +57,9 @@ namespace pip //point in polygon
 	template <typename T = Point>
 	bool equal(const T& p, const T& q)
 	{
-		if( !equal(p.x, q.x) ) return false;
-		if( !equal(p.y, q.y) ) return false;
-		if( !equal(p.z, q.z) ) return false;
+		if (!equal(p.x, q.x)) return false;
+		if (!equal(p.y, q.y)) return false;
+		if (!equal(p.z, q.z)) return false;
 
 		return true;
 	}
@@ -67,9 +67,9 @@ namespace pip //point in polygon
 	template <typename T = Point>
 	bool zero(const T& p)
 	{
-		if( !zero(p.x) ) return false;
-		if( !zero(p.y) ) return false;
-		if( !zero(p.z) ) return false;
+		if (!zero(p.x)) return false;
+		if (!zero(p.y)) return false;
+		if (!zero(p.z)) return false;
 
 		return true;
 	}
@@ -99,7 +99,7 @@ namespace pip //point in polygon
 	{
 		const auto m = magnitude(p.x, p.y, p.z);
 
-		if( m == 0. ) return {};
+		if (m == 0.) return {};
 
 		return { p.x / m , p.y / m , p.z / m };
 	}
@@ -107,9 +107,9 @@ namespace pip //point in polygon
 	template <typename T = Point>
 	double dot(const T& u, const T& v)
 	{
-		const auto dx = static_cast<double>(u.x * v.x);
-		const auto dy = static_cast<double>(u.y * v.y);
-		const auto dz = static_cast<double>(u.z * v.z);
+		const auto dx = static_cast<double>(u.x) * static_cast<double>(v.x);
+		const auto dy = static_cast<double>(u.y) * static_cast<double>(v.y);
+		const auto dz = static_cast<double>(u.z) * static_cast<double>(v.z);
 
 		return dx + dy + dz;
 	}
@@ -117,20 +117,20 @@ namespace pip //point in polygon
 	template <typename T = Point>
 	T cross(const T& u, const T& v)
 	{
-		return {u.y * v.z - u.z * v.y , u.z * v.x - u.x * v.z , u.x * v.y - u.y * v.x};
+		return { u.y * v.z - u.z * v.y , u.z * v.x - u.x * v.z , u.x * v.y - u.y * v.x };
 	}
 
 	enum class TurnDirection
 	{
-		Right  = 1,
-		Left   = -1,
+		Right = 1,
+		Left = -1,
 		NoTurn = 0
 	};
 
 	template <typename T = Point>
 	TurnDirection turn(const T& p, const T& u, const T& n, const T& q)
 	{
-		const T v = {q.x - p.x , q.y - p.y , q.z - p.z};
+		const T v = { q.x - p.x , q.y - p.y , q.z - p.z };
 
 		const auto dot = pip::dot(cross(v, u), n);
 
@@ -148,7 +148,7 @@ namespace pip //point in polygon
 	Position position(const T& p, const T& u, const T& n, const T& q)
 	{
 		const auto& north = u;
-		const auto  west  = cross(n, north);
+		const auto  west = cross(n, north);
 
 		const auto turn = pip::turn(p, west, n, q);
 
@@ -159,17 +159,17 @@ namespace pip //point in polygon
 	template <typename T = Point>
 	bool lineIntersect(const T& P1, const T& V1, const T& P2, const T& V2, double& mu1, double& mu2)
 	{
-		const T v13 = {P1.x - P2.x , P1.y - P2.y , P1.z - P2.z};
+		const T v13 = { P1.x - P2.x , P1.y - P2.y , P1.z - P2.z };
 
-		if( zero(v13) ) return true;
+		if (zero(v13)) return true;
 
 		const auto& v21 = V1;
 
-		if( zero(v21) ) return false;
+		if (zero(v21)) return false;
 
 		const auto& v43 = V2;
 
-		if( zero(v43) ) return false;
+		if (zero(v43)) return false;
 
 		const auto d1343 = dot(v13, v43);
 		const auto d4321 = dot(v43, v21);
@@ -179,7 +179,7 @@ namespace pip //point in polygon
 
 		const auto denom = d2121 * d4343 - d4321 * d4321;
 
-		if( denom == 0.0f ) return false; //Diverge from Paul Bourke
+		if (denom == 0.0f) return false; //Diverge from Paul Bourke
 
 		const auto numer = d1343 * d4321 - d1321 * d4343;
 
@@ -194,7 +194,7 @@ namespace pip //point in polygon
 	{
 		const auto n = polygon.size();
 
-		if( n < 2 ) return 0;
+		if (n < 2) return 0;
 
 		return equal(polygon.front(), polygon.back()) ? n - 1 : n;
 	}
@@ -206,9 +206,9 @@ namespace pip //point in polygon
 
 		const auto n = sizePolygon(polygon);
 
-		if( n < 3 ) return {};
+		if (n < 3) return {};
 
-		for( size_t i = 0; i < n; i++ )
+		for (size_t i = 0; i < n; i++)
 		{
 			const auto& item = polygon[i];
 			const auto& next = polygon[(i + 1) % n];
@@ -228,11 +228,11 @@ namespace pip //point in polygon
 
 		const auto n = sizePolygon(polygon);
 
-		if( n < 2 ) return {};
+		if (n < 2) return {};
 
 		float totalWeight(0.0f);
 
-		for( size_t i = 0; i < n; ++i )
+		for (size_t i = 0; i < n; ++i)
 		{
 			const auto& p1 = polygon[i];
 			const auto& p2 = polygon[(i + 1) % n];
@@ -258,7 +258,7 @@ namespace pip //point in polygon
 	{
 		const auto n = sizePolygon(polygon);
 
-		if( n < 2 ) return false;
+		if (n < 2) return false;
 
 		const auto normal = normalPolygon(polygon);
 
@@ -272,7 +272,7 @@ namespace pip //point in polygon
 
 		orientation = pip::normalize(orientation);
 
-		if( zero(orientation) )
+		if (pip::zero(orientation))
 		{
 			orientation.x = point.x - polygon[0].x;
 			orientation.y = point.y - polygon[0].y;
@@ -281,7 +281,7 @@ namespace pip //point in polygon
 			orientation = pip::normalize(orientation);
 		}
 
-		if( pip::zero(orientation) ) return false;
+		if (pip::zero(orientation)) return false;
 
 		double mus, mur;
 
@@ -290,7 +290,7 @@ namespace pip //point in polygon
 		bool negHit(false);
 		bool posHit(false);
 
-		for( size_t i = 0; i < n; i++ )
+		for (size_t i = 0; i < n; i++)
 		{
 			const auto& p1 = polygon[i];
 			const auto& p2 = polygon[(i + 1) % n];
@@ -301,14 +301,14 @@ namespace pip //point in polygon
 			u.y = p2.y - p1.y;
 			u.z = p2.z - p1.z;
 
-			if( pip::lineIntersect(p1, u, point, orientation, mus, mur) )
+			if (pip::lineIntersect<T>(p1, u, point, orientation, mus, mur))
 			{
-				if( mus >= 0.0 && mus <= 1.0 )
+				if (mus >= 0.0 && mus <= 1.0)
 				{
-					if( mur < 0.0 ) negHit = true;
-					if( mur > 0.0 ) posHit = true;
+					if (mur < 0.0) negHit = true;
+					if (mur > 0.0) posHit = true;
 
-					if( mur >= 0.0 )
+					if (mur >= 0.0)
 					{
 						intersectedSegments.push_back(i);
 						intersectedSegments.push_back(i + 1);
@@ -317,12 +317,12 @@ namespace pip //point in polygon
 			}
 		}
 
-		if( negHit && !posHit ) return false; //All hit behind  - > Outside
-		if( posHit && !negHit ) return false; //All hit in front - > Outside
+		if (negHit && !posHit) return false; //All hit behind  - > Outside
+		if (posHit && !negHit) return false; //All hit in front - > Outside
 
 		auto& is = intersectedSegments;
 
-		if( is.size() < 2 ) return false;
+		if (is.size() < 2) return false;
 
 		std::transform(is.begin(), is.end(), is.begin(), [n](size_t i) { return i % n; });
 
@@ -331,17 +331,17 @@ namespace pip //point in polygon
 
 		std::vector<std::vector<size_t>> sequenceIntersectedSegments;
 
-		while( item != tail )
+		while (item != tail)
 		{
 			auto next = item + 1;
 
-			if( next + 1 != tail )
+			if (next + 1 != tail)
 			{
-				while( *next == *(next + 1) )
+				while (*next == *(next + 1))
 				{
 					next = next + 2;
 
-					if( next + 1 == tail )
+					if (next + 1 == tail)
 						break;
 				}
 			}
@@ -353,24 +353,24 @@ namespace pip //point in polygon
 
 		size_t count(0);
 
-		for( auto& sequence : sequenceIntersectedSegments )
+		for (auto& sequence : sequenceIntersectedSegments)
 		{
-			if( sequence.size() < 2 )
+			if (sequence.size() < 2)
 				continue;
 
-			if( sequence.size() > 2 )
+			if (sequence.size() > 2)
 				sequence.erase(std::unique(sequence.begin(), sequence.end()), sequence.end());
 
 			auto turn = pip::turn(point, orientation, normal, polygon[sequence[0]]);
 
-			for( size_t i = 1; i < sequence.size(); i++ )
+			for (size_t i = 1; i < sequence.size(); i++)
 			{
 				const auto temp = pip::turn(point, orientation, normal, polygon[sequence[i]]);
 
-				if( temp == TurnDirection::NoTurn )
+				if (temp == TurnDirection::NoTurn)
 					continue;
 
-				if( temp == turn )
+				if (temp == turn)
 					continue;
 
 				count++;
